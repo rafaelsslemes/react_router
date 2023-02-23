2efc7de092afa67414c7761ab12f42467e185da7
Rafael Lemes <rafael.sslemes@gmail.com>
2023-02-23 12:49:41 -0300

    Http fetch products

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useFetch } from '../hooks/useFetch';
+import './Home.css';
 
 const Home = () => {
+
+  const backendURL = 'http://localhost:3000/products';
+  const {data, loading, errorMsg} = useFetch(backendURL);
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>Product List</h1>
+      <ul className='products'>
+        {errorMsg === null 
+          ? (data && data.map(product => (
+            <li key={product.id}>
+              <p>{product.name} R${product.price}</p>
+            </li>
+          )))
+          : (<h2>{errorMsg}</h2>)
+        }
+      </ul>
+
+    </div>
   )
 }
 
