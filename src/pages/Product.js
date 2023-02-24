ae149727712661f16635b80c5338c91ad8b31860
Rafael Lemes <rafael.sslemes@gmail.com>
2023-02-24 12:08:05 -0300

    Get single register by API Request

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useFetch } from '../hooks/useFetch';
 
 const Product = () => {
 
     const {id} = useParams();
+    const productURL = 'http://localhost:3000/products/' + id;
+
+    const {data : product, loading, errorMsg} = useFetch(productURL)
+
     return (
-        <div>Product {id}</div>
+        <div>
+            <h1>
+                Product {id}
+            </h1>
+            {loading && <h2>Loading product...</h2>}
+            {errorMsg && <h2>{errorMsg}</h2>}
+            {product && (
+                <div>
+                    <h2>{product.name}</h2>
+                    <h2>R$ {product.price}</h2>
+                </div>
+            )}
+        </div>
     )
 }
 
